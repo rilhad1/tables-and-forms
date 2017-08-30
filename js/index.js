d5b944b24cedd7c36198c4cd28a6f8d5ab3c189f
Volodumur Boychuk <rilhad1@gmail.com>
2017-08-30 11:18:56 +0000

    add new file

--- js/index.js
+++ js/index.js
@@ -14,14 +14,13 @@ function sendData(e) {
             pet: $('#pet').val(),
             settlement: $('input:radio[name=settlement]:checked').val(),
             settlementother: $('#settlement').val(),
-            hobby: $('input:checkbox:checked').val(function(){
-                if (input:checkbox:checked)
-                return name;}),
+            // hobby: $('input:checkbox:checked').val(function(){
+            //     if (input:checkbox:checked)
+            //     return name;}),
             transport: $('select#transport option:selected').val(),
         },
         dataType: "json"
     }).done(function() {
         $('form')[0].reset();
-        // $('#thanks').show();
     })
 }
